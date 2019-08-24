b3b3b6ac2822254dda3c44a399a40d01643dee5e
unknown <you@example.com>
2019-08-24 12:04:46 -0600

    nuevos cambios

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const config = {
+ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
@@ -19,13 +19,20 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image('control', 'assets/control.jpg');
+  this.load.image('control', 'assets/vector.png');
+  this.load.image('play', 'assets/boton.jpg');
 }
 
 function create(){
   const background = this.add.image(400, 300, 'control');
   background.displayWidth = game.config.width;
   background.displayHeight = game.config.height;
+
+  const play = this.add.image(400,530,'play');
+  play.displayWidth = 160;
+  play.displayHeight = 65;
+
+
 }
 
 function update() { }
