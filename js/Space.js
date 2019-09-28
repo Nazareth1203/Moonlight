36d100de172906722bc7da2a8dd48d99e4498e5b
César Bolaños <cesar.bolanos.andino@gmail.com>
2019-09-28 10:21:33 -0600

    Fix merge conflict

--- js/Space.js
+++ js/Space.js
@@ -1,35 +1,21 @@
 class Space extends Phaser.Scene {
-    constructor() {
-        super({ key: 'space' });
-    }
+  constructor() {
+    super({ key: 'space' });
+  }
 
-    preload() {
-      this.load.image('nave','assets/space/navenumber-one.png');
-    //  this.load.image('fondo', 'assets/space/estrellado.png');
-      this.load.image('bomba', 'assets/levels/bomba.png');
-      //this.load.image('bomba', 'assets/levels/bomba.png');
+  preload() {
+    this.load.image('nave','assets/space/navenumber-one.png');
+    this.load.image('bomba', 'assets/levels/bomba.png');
+  }
 
-    }
-
-    create() {
-
-    //  const background = this.add.image(400, 300, 'fondo');
-      //background.displayWidth = this.game.config.width;
-    //  background.displayHeight = this.game.config.height;
-
-   const nave = this.add.image(400, 520, 'nave');
+  create() {
+    const nave = this.add.image(400, 520, 'nave');
     nave.displayWidth = 100;
     nave.displayHeight = 145;
 
     const bomba = this.add.image(100, 250, 'bomba');
-     bombas.displayWidth = 50;
-     bombas.displayHeight = 90;
+    // bomba.displayWidth = 50;
+    // bomba.displayHeight = 90;
 
-     //const bomba = this.add.image(90, 240, 'bomba');
-      //bombas.displayWidth = 50;
-      //bombas.displayHeight = 90;
-
-
-
-    }
+  }
 }
