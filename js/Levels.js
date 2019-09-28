200eb0805149af1e6c49fc40ef60de51dca32b27
unknown <you@example.com>
2019-09-28 10:44:05 -0600

    Fix merge conflicts

--- js/Levels.js
+++ js/Levels.js
@@ -6,13 +6,8 @@ class Levels extends Phaser.Scene {
   preload() {
     this.load.image('background','assets/levels/background.png');
     this.load.image('number-one','assets/levels/number-one.png');
-<<<<<<< HEAD
-    //this.load.image('number-two','assets/levels/number-two.png');
-
-=======
     this.load.image('number-two','assets/levels/number-two.png');
     this.load.image('number-tree','assets/levels/number-tree.png');
->>>>>>> d9590f5c843f093c82c4c73ce3146e073419862f
   }
 
   create() {
@@ -51,12 +46,6 @@ class Levels extends Phaser.Scene {
     numberOne.setInteractive();
     numberOne.once('pointerup', () => this.scene.start('space'), this);
 
-<<<<<<< HEAD
-    //const numberTwo = this.add.image(400, 530, 'number-two');
-    //numberOne.displayWidth = 80;
-    //numberOne.displayHeight = 80;
-    //numberOne.setInteractive();
-=======
     //nunber-two
     const numbertwo = this.add.image(625, 340, 'number-two');
     numbertwo.displayWidth = 80;
@@ -69,6 +58,5 @@ class Levels extends Phaser.Scene {
     numbertree.displayHeight = 80;
     numbertree.setInteractive();
     numbertree.once('pointerup', () => this.scene.start('nevel'), this);
->>>>>>> d9590f5c843f093c82c4c73ce3146e073419862f
   }
 }
