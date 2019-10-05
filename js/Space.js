3cde381767e9755ca17929d26ed1e3a0c2883a4c
unknown <you@example.com>
2019-10-05 09:05:29 -0600

    Cambios

--- js/Space.js
+++ js/Space.js
@@ -5,7 +5,16 @@ class Space extends Phaser.Scene {
 
   preload() {
     this.load.image('nave','assets/space/navenumber-one.png');
-    this.load.image('bomba', 'assets/levels/bomba.png');
+    this.load.image('bomb', 'assets/space/bomb.png');
+    this.load.image('bomb', 'assets/space/bomb.png');
+    this.load.image('bomb', 'assets/space/bomb.png');
+    this.load.image('bomb', 'assets/space/bomb.png');
+    this.load.image('bomb', 'assets/space/bomb.png');
+    this.load.image('bomb', 'assets/space/bomb.png');
+    this.load.image('bomb', 'assets/space/bomb.png');
+    this.load.image('bomb', 'assets/space/bomb.png');
+    this.load.image('bomb', 'assets/space/bomb.png');
+    this.load.image('bomb', 'assets/space/bomb.png');
   }
 
   create() {
@@ -14,8 +23,21 @@ class Space extends Phaser.Scene {
     this.ship.displayHeight = 145;
     this.ship.setCollideWorldBounds(true);
 
-    this.add.image(100, 250, 'bomba');
-    
+    this.add.image(100, 250, 'bomb');
+    this.add.image(150, 290, 'bomb');
+    this.add.image(200, 250, 'bomb');
+    this.add.image(250, 250, 'bomb');
+    this.add.image(300, 280, 'bomb');
+    this.add.image(350, 250, 'bomb');
+    this.add.image(400, 260, 'bomb');
+    this.add.image(450, 250, 'bomb');
+    this.add.image(500, 295, 'bomb');
+    this.add.image(550, 280, 'bomb');
+    this.add.image(600, 250, 'bomb');
+    this.add.image(650, 265, 'bomb');
+
+
+
     this.cursors = this.input.keyboard.createCursorKeys();
   }
 
