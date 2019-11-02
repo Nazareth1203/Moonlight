0043177d0b7245125b9f94bf724f40e5bcfa49e5
unknown <you@example.com>
2019-11-02 12:06:21 -0600

    ultimos cambios

--- js/Levels.js
+++ js/Levels.js
@@ -4,12 +4,12 @@ class Levels extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('background','assets/levels/background.png');
+    this.load.image('background','assets/levels/nivel.png');
     this.load.image('number-one','assets/levels/number-one.png');
     this.load.image('number-two','assets/levels/number-two.png');
     this.load.image('number-tree','assets/levels/number-tree.png');
-    this.load.image('number-four','assets/levels/number-four.png');
-    this.load.image('number-five','assets/levels/number-five.png');
+    //this.load.image('number-four','assets/levels/number-four.png');
+    //this.load.image('number-five','assets/levels/number-five.png');
   }
 
   create() {
@@ -23,54 +23,54 @@ class Levels extends Phaser.Scene {
 
     // primero circulo:
     graphics.fillStyle(color, alpha);
-    graphics.fillCircle(400, 150, 40);
+    graphics.fillCircle(490, 220, 80);
 
     // segundo circulo:
     graphics.fillStyle(color, alpha);
-    graphics.fillCircle(400, 350, 100);
+    graphics.fillCircle(280, 340, 80);
 
     // tercer circulo:
     graphics.fillStyle(color, alpha);
-    graphics.fillCircle(400, 530, 40);
+    graphics.fillCircle(500, 460, 80);
 
-     cuarto circulo:
-    graphics.fillStyle(color, alpha);
-    graphics.fillCircle(625, 340, 40);
+     //cuarto circulo:
+  //  graphics.fillStyle(color, alpha);
+    //graphics.fillCircle(625, 340, 40);
 
   //  quinto circulo:
-   graphics.fillStyle(color, alpha);
-    graphics.fillCircle(165, 340, 40);
+   //graphics.fillStyle(color, alpha);
+    //graphics.fillCircle(165, 340, 40);
 
     //number-one
-    const numberOne = this.add.image(400, 530, 'number-one');
-    numberOne.displayWidth = 80;
-    numberOne.displayHeight = 80;
+    const numberOne = this.add.image(490, 210, 'number-one');
+    numberOne.displayWidth = 90;
+    numberOne.displayHeight = 90;
     numberOne.setInteractive();
     numberOne.once('pointerup', () => this.scene.start('space'), this);
 
     //nunber-two
-    const numbertwo = this.add.image(625, 340, 'number-two');
-    numbertwo.displayWidth = 80;
-    numbertwo.displayHeight = 80;
-    numbertwo.setInteractive();
-    numbertwo.once('pointerup', () => this.scene.start('nevel'), this);
+  const numbertwo = this.add.image(280, 340, 'number-two');
+  numbertwo.displayWidth = 80;
+  numbertwo.displayHeight = 80;
+  numbertwo.setInteractive();
+  numbertwo.once('pointerup', () => this.scene.start('nevel'), this);
 
-    const numbertree = this.add.image(400, 150, 'number-tree');
-    numbertree.displayWidth = 80;
-    numbertree.displayHeight = 80;
+    const numbertree = this.add.image(500, 460, 'number-tree');
+    numbertree.displayWidth =90;
+    numbertree.displayHeight = 90;
     numbertree.setInteractive();
     numbertree.once('pointerup', () => this.scene.start('nivel'), this);
 
-  const numberfour = this.add.image(160, 340, 'number-four');
-    numberfour.displayWidth = 80;
-    numberfour.displayHeight = 80;
-    numberfour.setInteractive();
-    numberfour.once('pointerup', () => this.scene.start('nivel4'), this);
+  //const numberfour = this.add.image(160, 340, 'number-four');
+    //numberfour.displayWidth = 80;
+    //numberfour.displayHeight = 80;
+    //numberfour.setInteractive();
+    //numberfour.once('pointerup', () => this.scene.start('nivel4'), this);
 
-    const numberfive = this.add.image(400, 350, 'number-five');
-    numberfive.displayWidth = 100;
-    numberfive.displayHeight = 100;
-    numberfive.setInteractive();
-    numberfive.once('pointerup', () => this.scene.start('final'), this);
+    //const numberfive = this.add.image(400, 350, 'number-five');
+    //numberfive.displayWidth = 80;
+    //numberfive.displayHeight = 80;
+    //numberfive.setInteractive();
+    //numberfive.once('pointerup', () => this.scene.start('final'), this);
   }
 }
